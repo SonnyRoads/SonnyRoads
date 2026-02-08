fdc07b609a5b8a0178920b9b8933ab735d760b43
SonnyRoads <kaya.crows@gmail.com>
2026-02-08 12:00:51 -0600

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,21 +1,33 @@
 window.addEventListener('DOMContentLoaded', () => {
+  const man = document.querySelector('.man');
+  const sign = document.querySelector('.sign-container');
   const scroll = document.querySelector('.scroll-video');
   const startBtn = document.querySelector('.start');
 
-  // Fade in scroll after 5 seconds and play
+  // Fade in pointing.png at 3 seconds
+  setTimeout(() => {
+    man.style.opacity = '1';
+  }, 3000);
+
+  // Fade in sign.mp4 at 4 seconds
+  setTimeout(() => {
+    sign.style.opacity = '1';
+  }, 4000);
+
+  // Fade in scroll.mp4 at 6 seconds and start playing
   setTimeout(() => {
     scroll.style.opacity = '1';
     scroll.play();
-  }, 5000);
+  }, 6000);
 
-  // Fade in Begin Journey button after 6 seconds
+  // Fade in Begin Journey button at 8 seconds
   setTimeout(() => {
     startBtn.style.opacity = '1';
-  }, 6000);
+  }, 8000);
 
   // Begin Journey button click handler
   startBtn.addEventListener('click', () => {
     console.log('Journey begins!');
-    // Here you can add next actions, e.g., fade to black, play video
+    // Here you can add your next actions (e.g., fade to black, play next video)
   });
 });
