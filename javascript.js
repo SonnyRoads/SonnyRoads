bdada853f66b35576bc32924bdf48c7d834bd11d
SonnyRoads <kaya.crows@gmail.com>
2026-02-09 16:11:58 -0600

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,10 +1,13 @@
 window.addEventListener('DOMContentLoaded', () => {
   const video = document.getElementById('vid');
 
-  // wait until browser is READY to play
   video.addEventListener('loadedmetadata', () => {
     video.play().catch(err => {
       console.log('Play blocked:', err);
     });
   });
+
+  setTimeout(() => {
+    video.style.opacity = '1';
+  }, 2000);
 });
