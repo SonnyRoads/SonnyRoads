fb55bf814f7a3997c5df3098743b1c1ac59bf12a
SonnyRoads <kaya.crows@gmail.com>
2026-02-09 17:40:42 -0600

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,7 +1,11 @@
-window.addEventListener("DOMContentLoaded", () => {
-  const btn = document.getElementById("begin-btn");
+document.addEventListener("DOMContentLoaded", () => {
+  const btn = document.getElementById("startBtn");
 
+  console.log("JS LOADED");
+
+  // Safety: force visibility after 2s
   setTimeout(() => {
     btn.style.opacity = "1";
+    btn.style.display = "block";
   }, 2000);
 });
